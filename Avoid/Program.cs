359e4f69fa301f9da12c4e4c4c3bef5e46f2822d
Fifow1 <lasldjf123@naver.com>
2024-12-06 15:13:31 +0900

    코드 업로드

--- Avoid/Program.cs
+++ Avoid/Program.cs
@@ -1,16 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System;
 
-namespace Avoid
+namespace Day11
 {
     internal class Program
     {
+        struct Vector2
+        {
+            public int x;
+            public int y;
+
+        }
+
+        struct Bullet
+        {
+            public int posX;
+            public int posY;
+            public bool isFired;
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.CursorVisible = false; //커서 안보이게
+
+            ConsoleKeyInfo temp; //키입력 기억할 변수
+            Stopwatch watch = new Stopwatch(); //이 한줄 적으면 스탑워치가 만들어진다
+            watch.Start(); //스탑워치 시작함
+
+            Vector2 playerPos;
+
+            playerPos.x = 0;
+            playerPos.y = 0;
+
+            // 값 초기화
+            bool isFired = false;
+            int posX = 0;
+            int posY = 0;
+
+
+
+            while (true)
+            {
+
+
+                //=================입력만!!!=====================
+                if (Console.KeyAvailable) //키가 눌렸을때만 참
+                {
+                    temp = Console.ReadKey(true); //키입력이 담김
+                    if (temp.Key == ConsoleKey.Spacebar) //스페이스 바 눌리면 true로 바꾸기
+                    {
+                        if (isFired == true)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            posX = playerPos.x;
+                            posY = playerPos.y;
+                            isFired = true;
+                        }
+                    }
+
+                    else if (temp.Key == ConsoleKey.A)
+                    {
+                        playerPos.x -= 2;
+                    }
+
+                    else if (temp.Key == ConsoleKey.D)
+                    {
+                        playerPos.x += 2;
+                    }
+                    else if (temp.Key == ConsoleKey.W)
+                    {
+                        playerPos.y--;
+                    }
+                    else if (temp.Key == ConsoleKey.S)
+                    {
+                        playerPos.y++;
+                    }
+                }
+
+
+
+
+
+                //===========================================로직 처리
+                if (watch.ElapsedMilliseconds >= 2000)
+                {
+
+                    if (isFired == true)
+                    {
+                        posX += 2;
+                        if (posX > 100)
+                        {
+                            isFired = false;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    watch.Restart();
+                }
+                //==============================================그리는 부분==================
+
+                Console.SetCursorPosition(playerPos.x, playerPos.y);
+                Console.Write("◈");
+
+                // foreach (var a in bullets)
+                //  {
+                Console.SetCursorPosition(posX, posY);
+                Console.Write("▶");
+                //   }
+
+
+
+
+            }
         }
     }
 }
---
+++ AvoidTest/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ldjlajljasldjflskfjwon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■□□□□□□□□□□□□□□□□□□□□□□□□□□□□■");
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+
+            ConsoleKeyInfo inputKey;
+
+
+            // 30
+            int consoleX = 1;
+
+            // 30
+            int consoleY = 1;
+
+            int hurt = 4;
+
+            Random random = new Random();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            long endScore = 100000000000000000;
+            long playerScore = 0;
+
+
+
+
+
+            watch.Start();
+            int aaX = 2;
+            int aaY = 1;
+            string gunChar = "▶";
+            bool isFired = false;
+
+
+            while (playerScore < endScore)
+            {
+                //  playerScore = (long)watch.ElapsedMilliseconds;
+                // Console.WriteLine(playerScore);
+                Console.SetCursorPosition(consoleX * 2, consoleY);
+                Console.Write("●");
+
+                if (watch.ElapsedMilliseconds >= 160)
+                {
+                    watch.Restart();
+                    if (Console.KeyAvailable) //키가 눌렸을때만 참
+                    {
+                        inputKey = Console.ReadKey(true);
+
+
+                        switch (inputKey.Key)
+                        {
+
+                            //  -▶
+                            case ConsoleKey.RightArrow:
+                                isFired = true;
+                                consoleX++;
+                                Console.SetCursorPosition((consoleX - 1) * 2, consoleY);
+                                Console.Write("□");
+                                break;
+
+
+
+                            // ◀-
+                            case ConsoleKey.LeftArrow:
+                                isFired = true;
+                                consoleX--;
+                                Console.SetCursorPosition((consoleX + 1) * 2, consoleY);
+                                Console.Write("□");
+                                break;
+
+
+
+
+                            // ▲
+                            // ㅣ
+                            case ConsoleKey.UpArrow:
+                                isFired = true;
+                                consoleY--;
+                                Console.SetCursorPosition(consoleX * 2, consoleY + 1);
+                                Console.Write("□");
+                                break;
+
+
+
+                            // ㅣ
+                            // ▼
+                            case ConsoleKey.DownArrow:
+                                isFired = true;
+                                consoleY++;
+                                Console.SetCursorPosition(consoleX * 2, consoleY - 1);
+                                Console.Write("□");
+                                break;
+
+
+                        }
+                        // 왼쪽 밖으로 못나가게
+                        if (consoleX < 1)
+                        {
+                            consoleX = 1;
+                        }
+
+                        // 위쪽 밖으로 못나가게
+                        else if (consoleY < 1)
+                        {
+
+                            consoleY = 1;
+                        }
+
+                        // 오른쪽 밖으로 못나가게
+                        else if (consoleX > 28)
+                        {
+                            consoleX = 28;
+                        }
+
+                        // 아래쪽 밖으로 못나가게
+                        else if (consoleY > 28)
+                        {
+                            consoleY = 28;
+                        }
+                    }
+
+                    if (isFired == true)
+                    {
+                        Console.SetCursorPosition(aaX, aaY);
+                        Console.WriteLine("　");
+                        aaX += 2;
+                        Console.SetCursorPosition(aaX, aaY);
+                        Console.Write(gunChar);
+                        if (aaX > 54)
+                        {
+                            Console.SetCursorPosition(aaX, aaY);
+                            Console.Write("  ");
+                            break;
+                        }
+
+                    }
+                }
+
+
+            }
+            while (true)
+            {
+
+            }
+        }
+    }
+}
